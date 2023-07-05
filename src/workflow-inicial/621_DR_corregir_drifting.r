3828c7fafba3c09531e75692d3ccdfa820e5ef50
rstvz90 <rodriestevez@hotmail.com>
2023-07-04 23:29:54 -0300

    se cambia a ranking cero fijo

--- src/workflow-inicial/621_DR_corregir_drifting.r
+++ src/workflow-inicial/621_DR_corregir_drifting.r
@@ -19,7 +19,7 @@ PARAM$variables_intrames <- TRUE # atencion esto esta en TRUE
 
 # valores posibles
 #  "ninguno", "rank_simple", "rank_cero_fijo", "deflacion"
-PARAM$metodo <- "deflacion"
+PARAM$metodo <- "rank_cero_fijo"
 
 PARAM$home <- "~/buckets/b1/"
 # FIN Parametros del script
