b198fb9b86c842921a87a797735b87328f949a22
rstvz90 <rodriestevez@hotmail.com>
2023-07-14 21:39:25 -0300

    se vuelve a intentar el exp 3

--- src/workflow-inicial/611_CA_reparar_dataset.r
+++ src/workflow-inicial/611_CA_reparar_dataset.r
@@ -10,7 +10,7 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "CA6110_exp5"
+PARAM$experimento <- "CA6110_exp3"
 PARAM$dataset <- "./datasets/competencia_2023.csv.gz"
 
 # valores posibles
--- src/workflow-inicial/651_HT_lightgbm.r
+++ src/workflow-inicial/651_HT_lightgbm.r
@@ -27,9 +27,9 @@ options(error = function() {
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "HT6510_exp5"
+PARAM$experimento <- "HT6510_exp3"
 
-PARAM$exp_input <- "TS6410_exp5"
+PARAM$exp_input <- "TS6410_exp3"
 
 # En caso que se haga cross validation, se usa esta cantidad de folds
 PARAM$lgb_crossvalidation_folds <- 5
