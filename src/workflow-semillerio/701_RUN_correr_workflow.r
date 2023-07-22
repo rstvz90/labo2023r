24df0a61a193e89692ec22dd8d776c913cf3b361
rstvz90 <rodriestevez@hotmail.com>
2023-07-22 11:26:27 -0300

    se cambia a exp1

--- src/workflow-semillerio/701_RUN_correr_workflow.r
+++ src/workflow-semillerio/701_RUN_correr_workflow.r
@@ -8,9 +8,9 @@ options(error = function() {
 
 
 # corrida de cada paso del workflow
-source("~/labo2023r/src/workflow-semillerio/711_CA_reparar_dataset.r")
-source("~/labo2023r/src/workflow-semillerio/721_DR_corregir_drifting.r")
-source("~/labo2023r/src/workflow-semillerio/731_FE_historia.r")
-source("~/labo2023r/src/workflow-semillerio/741_TS_training_strategy.r")
-source("~/labo2023r/src/workflow-semillerio/751_HT_lightgbm.r")
-source("~/labo2023r/src/workflow-semillerio/771_ZZ_final_semillerio.r")
+source("~/labo2023r/src/workflow-semillerio/711_CA_reparar_dataset_exp1.r")
+source("~/labo2023r/src/workflow-semillerio/721_DR_corregir_drifting_exp1.r")
+source("~/labo2023r/src/workflow-semillerio/731_FE_historia_exp1.r")
+source("~/labo2023r/src/workflow-semillerio/741_TS_training_strategy_exp1.r")
+source("~/labo2023r/src/workflow-semillerio/751_HT_lightgbm_exp1.r")
+source("~/labo2023r/src/workflow-semillerio/771_ZZ_final_semillerio_exp1.r")
