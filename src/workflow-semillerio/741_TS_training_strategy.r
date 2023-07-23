438665e5d395146af6c1b7c3265be390a4e059a0
rstvz90 <rodriestevez@hotmail.com>
2023-07-23 15:23:39 -0300

    se cambia a experimento 2

--- src/workflow-semillerio/741_TS_training_strategy.r
+++ src/workflow-semillerio/741_TS_training_strategy.r
@@ -11,9 +11,9 @@ require("yaml")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "TS7410_exp1"
+PARAM$experimento <- "TS7410_exp2"
 
-PARAM$exp_input <- "FE7310_exp1"
+PARAM$exp_input <- "FE7310_exp2"
 
 # me salteo los meses duros de pandemia, pero llego hasta 201907 en training
 # entreno en 18 meses
