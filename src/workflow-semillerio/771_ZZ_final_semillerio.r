63261ffd53e896bec5f6039312bc2a4ed639753d
rstvz90 <rodriestevez@hotmail.com>
2023-07-28 07:41:27 -0300

    sem6, se cambia a 15 semillas por una cuestion de tiempo

--- src/workflow-semillerio/771_ZZ_final_semillerio.r
+++ src/workflow-semillerio/771_ZZ_final_semillerio.r
@@ -20,8 +20,8 @@ require("lightgbm")
 
 # Parametros del script
 PARAM <- list()
-PARAM$experimento <- "ZZ7710_sem5"
-PARAM$exp_input <- "HT7510_sem5"
+PARAM$experimento <- "ZZ7710_sem6"
+PARAM$exp_input <- "HT7510_sem6"
 
 # Que modelos quiero, segun su posicion en el ranking
 # de la Bayesian Optimizacion, ordenado por ganancia descendente
@@ -30,7 +30,7 @@ PARAM$modelos_rank <- c(1)
 # cantidad de semillas a utilizar en el semillerio,
 #  podrian ser 50 o 100 para mayor estabilidad
 #  semillerio <- 1  es el ZZ_final  tradicional
-PARAM$semillerio <- 20
+PARAM$semillerio <- 15
 
 # se utiliza para generar el vector de  PARAM$semillerio  semillas
 PARAM$semilla_primos <- 102013
